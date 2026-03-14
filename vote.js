c385bb7bb2bed3fd2f7030be7acab090f2a3cb00
suhas45gowda <suhas45gowda@gmail.com>
2026-03-14 10:52:29 +0530

    Add files via upload
    hi

---
+++ vote.js
@@ -0,0 +1,10 @@
+function vote(){
+    var name = document.getElementById("name").Value;
+    var name = document.getElementById("age").Value;
+    var name = document.getElementById("res").Value;
+}
+    if (age>=18){
+        res.innerHTML =NAME+""+"eligible to vote"
+    }else{
+         res.innerHTML =NAME+""+"you are still child"
+    }
